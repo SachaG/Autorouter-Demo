b90b177e96b0a20f1d5f4d082f1e7b3d25d609c4
Sacha Greif <sacha357@gmail.com>
2015-01-19 09:12:33 +0900

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-Posts = new Meteor.Collection("posts");
+Posts = new Mongo.Collection("posts");
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
--- packages/autorouting/autorouting.js
+++
@@ -1,8 +0,0 @@
-Router.route('/', function () {
-  this.render('main', {
-    data: function () {
-      return {foo: "bar"}
-      return Posts.findOne({_id: this.params._id});
-    }
-  });
-});
